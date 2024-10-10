2d6b7918d446e191908012e226272107d61df23b
kornman00 <kornman00@gmail.com>
2024-10-10 11:39:56 -0700

    Setup C++ support

---
+++ Source/LearningAgentsDrive.Target.cs
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class LearningAgentsDriveTarget : TargetRules
+{
+	public LearningAgentsDriveTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+
+		ExtraModuleNames.AddRange(new string[] {
+			"LearningAgentsDrive",
+		});
+	}
+}
---
+++ Source/LearningAgentsDrive/LearningAgentsDrive.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LearningAgentsDrive.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LearningAgentsDrive, "LearningAgentsDrive" );
